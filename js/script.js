1a1bc46b29932107bcdd5428a8420d4d21ee5cfc
WahidaAkhter <wahidacse280@gmail.com>
2022-02-17 09:22:20 +0600

    expense and balance calculation done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 
 function calculate() {
-    let incomevalue = document.getElementById("income-input").value;
+    // total expenses 
+    let foodValue = document.getElementById("food-input").value;
+    let rentValue = document.getElementById("rent-input").value;
+    let clothesValue = document.getElementById("clothes-input").value;
+    let totalValue = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothesValue);
+    let totalExpenseInput = document.getElementById("total-expense");
+    totalExpenseInput.innerText = totalValue;
+    // total balance
+    let incomeValue = document.getElementById("income-input").value;
+    let balanceInput = document.getElementById("balance");
+    let totalBalance = parseInt(incomeValue) - totalValue;
+    balanceInput.innerText = totalBalance;
 
-    console.log(incomevalue);
-
+}
+function save() {
+    let savingValue = document.getElementById("saving-input");
+    savingValue.innerText = 10000 * 0.2;
 }
