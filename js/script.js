fc9935b37eae1ee132d13d1d1038cfb43e8d13e9
WahidaAkhter <wahidacse280@gmail.com>
2022-02-17 18:08:44 +0600

    js error added

--- js/script.js
+++ js/script.js
@@ -11,6 +11,74 @@ function Aboutbalance() {
     return balanceInput.innerText;
 }
 
+// error case handling for negative number
+
+function varifyError() {
+    let incomeValue = document.getElementById("income-input").value;
+    let foodValue = document.getElementById("food-input").value;
+    let rentValue = document.getElementById("rent-input").value;
+    let clothesValue = document.getElementById("clothes-input").value;
+    let savingValue = document.getElementById("save-input").value;
+
+    if (parseInt(foodValue) < 0) {
+        alert("please enter valid number");
+
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+    if (parseInt(rentValue) < 0) {
+        alert("please enter valid number");
+
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+    if (parseInt(clothesValue) < 0) {
+        alert("please enter valid number");
+
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+    if (parseInt(incomeValue) < 0) {
+        alert("please enter valid number");
+
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+    if (parseInt(savingValue) < 0) {
+        alert("please enter valid number");
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+
+    // error handling part 2 if expenses > income
+
+    if (parseInt(document.getElementById("total-expense").innerText) > parseInt(document.getElementById("income-input").value)) {
+        alert("you expended too much!!")
+        document.getElementById("balance").innerText = "";
+        document.getElementById("total-expense").innerText = "";
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+
+    // error handling part 3 about save percentage
+
+    if (parseInt(savingValue) > 100) {
+        alert("oh! you cant save that much");
+        document.getElementById("saving-output").innerText = "";
+        document.getElementById("remaining-input").innerText = "";
+    }
+}
+
 function calculate() {
 
     // total expenses
@@ -30,11 +98,14 @@ function calculate() {
     balanceInput.innerText = totalBalance;
 
     // clearing input value
-    foodValue = "";
-    rentValue = "";
-    clothesValue = "";
-    incomeValue = "";
+    document.getElementById("income-input").innerText = "";
+    document.getElementById("food-input").innerText = "";
+    document.getElementById("rent-input").innerText = "";
+    document.getElementById("clothes-input").innerText = "";
+
+    varifyError();
 }
+
 function save() {
 
     // saving percentage
@@ -57,7 +128,7 @@ function save() {
     remainingBalanceInput.innerText = Aboutbalance() - savingAmount;
 
     //    clean up
-    savingValue.value = "";
+    document.getElementById("save-input").innerTex = "";
 
-
+    varifyError();
 }
